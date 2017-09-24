96a00e0210ca61e78934f43732c21303652d0695
tushighen <tushig.0803@gmail.com>
2017-09-25 01:57:24 +0800

    asd

--- php/calculator.php
+++ php/calculator.php
@@ -1,47 +1,46 @@
 <?php
-    $constOutWarm = 0.0366;
-    $constOnePersonPerWarm = 0.212;
-    $constHotWaterPower = 0.0467;
-    $constColdWaterFromTheEarth = 5;
-    $constDHWWater = 50;
+$constOutWarm = 0.0366;
+$constOnePersonPerWarm = 0.212;
+$constHotWaterPower = 0.0467;
+$constColdWaterFromTheEarth = 5;
+$constDHWWater = 50;
 
-    if (isset($_POST['calcForm'])) {
+if (isset($_POST['calcForm'])) {
 
-        $allBuild = $_POST['allBuild'];
-        $allSquare = $_POST['allSquare'];
-        $pplNum = $_POST['pplNum'];
-        $neededEner = $_POST['neededEner'];
-        $perPower = $_POST['perPower'];
+    $allBuild = $_POST['allBuild'];
+    $allSquare = $_POST['allSquare'];
+    $pplNum = $_POST['pplNum'];
+    $neededEner = $_POST['neededEner'];
+    $perPower = $_POST['perPower'];
 
-        $dayWorkTime = 12;
+    $dayWorkTime = 12;
 
-        $e20 = $allSquare * $constOutWarm; // Семья из 3 чел. в день 237 л.
+    $e20 = $allSquare * $constOutWarm; // Семья из 3 чел. в день 237 л.
 
-        $type = $_POST['type'];
-        $getCoff = mysql_query("select coefficient as coff from tableMaterial WHERE id=$type");
-        $row = mysql_fetch_assoc($getCoff);
-        $coff = $row['coff'];
+    $type = $_POST['type'];
+    $getCoff = mysql_query("select coefficient as coff from tableMaterial WHERE id=$type");
+    $row = mysql_fetch_assoc($getCoff);
+    $coff = $row['coff'];
 
-        $warmPerMonth = $e20 / 0.8598 * $coff;
-        $spentWaterInaMonth = $pplNum * 31 * 79;
+    $warmPerMonth = $e20 / 0.8598 * $coff;
+    $spentWaterInaMonth = $pplNum * 31 * 79;
 
-        $e19 = $spentWaterInaMonth / 1000 * $constHotWaterPower; // Семья из 3 чел. в день 237 л.
+    $e19 = $spentWaterInaMonth / 1000 * $constHotWaterPower; // Семья из 3 чел. в день 237 л.
 
-        $mvtPerMonth = $e19 * 1.163;
+    $mvtPerMonth = $e19 * 1.163;
 
-        $enEv = $warmPerMonth / 31 / 24 * 1000 * (1 + $allBuild / 150) + $mvtPerMonth / 31 /24 * 1000;
-        $nasosTotCosMonth = $enEv / 20 * 24 * 31 / 1000 + $enEv / 40 * $dayWorkTime * 31 / 1000;
-        $abc = (integer)$enEv;
-        $abz = round($abc, -1);
+    $enEv = $warmPerMonth / 31 / 24 * 1000 * (1 + $allBuild / 150) + $mvtPerMonth / 31 / 24 * 1000;
+    $nasosTotCosMonth = $enEv / 20 * 24 * 31 / 1000 + $enEv / 40 * $dayWorkTime * 31 / 1000;
+    $enEv2 = (integer)$enEv;
+    $enEv2_1 = round($enEv2, -1);
+    if ($enEv2_1 > $enEv2)
+        $enEv2_1 -= 10;
 
-        $getPwr = mysql_query("select hpgkw as hp from tableNumbers WHERE hpgkw = $abc");
-        $row = mysql_fetch_assoc($getPwr);
-        $neededPwr = $row['hp'];
+    $getPwr = mysql_query("select hpgkw as hp from tableNumbers WHERE hpgkw = $enEv2_1");
+    $row = mysql_fetch_assoc($getPwr);
+    $neededPwr = $row['hp'];
 
-        header("Location: 200k.php?selected=$type&hotwater=$neededPwr");
+    header("Location: 200k.php?selected=$type&hotwater=$neededPwr");
 
-        function abc($a) {
-
-        }
-    }
+}
 ?>
