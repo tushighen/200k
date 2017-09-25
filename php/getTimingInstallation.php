4b3d74bfaca3ef81bdfdcf8bfebba6e6660acc8c
tushighen <tushig.0803@gmail.com>
2017-09-25 20:02:42 +0800

    asd

--- php/getTimingInstallation.php
+++ php/getTimingInstallation.php
@@ -1,6 +1,107 @@
 <?php
-function getTimingInstallation() {
-    if ($m2 => "84" && $m2<= "520" )
-        $bymba = 1;
-    }
+function getTimingInstallation($m2)
+{
+    if ($m2 >= 84 && $m2 <= 520)
+        $returnValue = 1;
+    elseif ($m2 >= 690 && $m2 <= 1170)
+        $returnValue = 2;
+    elseif ($m2 >= 690 && $m2 <= 1170)
+        $returnValue = 2;
+    elseif ($m2 >= 690 && $m2 <= 1170)
+        $returnValue = 2;
+    elseif ($m2 >= 690 && $m2 <= 1170)
+        $returnValue = 2;
+    elseif ($m2 >= 690 && $m2 <= 1170)
+        $returnValue = 2;
+    elseif ($m2 >= 690 && $m2 <= 1170)
+        $returnValue = 2;
+    elseif ($m2 >= 690 && $m2 <= 1170)
+        $returnValue = 2;
+    elseif ($m2 >= 690 && $m2 <= 1170)
+        $returnValue = 2;
+    elseif ($m2 >= 690 && $m2 <= 1170)
+        $returnValue = 2;
+    elseif ($m2 >= 690 && $m2 <= 1170)
+        $returnValue = 2;
+    elseif ($m2 >= 690 && $m2 <= 1170)
+        $returnValue = 2;
+    elseif ($m2 >= 690 && $m2 <= 1170)
+        $returnValue = 2;
+    elseif ($m2 >= 690 && $m2 <= 1170)
+        $returnValue = 2;
+    elseif ($m2 >= 690 && $m2 <= 1170)
+        $returnValue = 2;
+    elseif ($m2 >= 690 && $m2 <= 1170)
+        $returnValue = 2;
+    elseif ($m2 >= 690 && $m2 <= 1170)
+        $returnValue = 2;
+    elseif ($m2 >= 690 && $m2 <= 1170)
+        $returnValue = 2;
+    elseif ($m2 >= 690 && $m2 <= 1170)
+        $returnValue = 2;
+    elseif ($m2 >= 690 && $m2 <= 1170)
+        $returnValue = 2;
+    elseif ($m2 >= 690 && $m2 <= 1170)
+        $returnValue = 2;
+    elseif ($m2 >= 690 && $m2 <= 1170)
+        $returnValue = 2;
+    elseif ($m2 >= 690 && $m2 <= 1170)
+        $returnValue = 2;
+    elseif ($m2 >= 690 && $m2 <= 1170)
+        $returnValue = 2;
+    elseif ($m2 >= 690 && $m2 <= 1170)
+        $returnValue = 2;
+    elseif ($m2 >= 690 && $m2 <= 1170)
+        $returnValue = 2;
+    elseif ($m2 >= 690 && $m2 <= 1170)
+        $returnValue = 2;
+    elseif ($m2 >= 690 && $m2 <= 1170)
+        $returnValue = 2;
+    elseif ($m2 >= 690 && $m2 <= 1170)
+        $returnValue = 2;
+    elseif ($m2 >= 690 && $m2 <= 1170)
+        $returnValue = 2;
+    elseif ($m2 >= 690 && $m2 <= 1170)
+        $returnValue = 2;
+    elseif ($m2 >= 690 && $m2 <= 1170)
+        $returnValue = 2;
+    elseif ($m2 >= 690 && $m2 <= 1170)
+        $returnValue = 2;
+    elseif ($m2 >= 690 && $m2 <= 1170)
+        $returnValue = 2;
+    elseif ($m2 >= 690 && $m2 <= 1170)
+        $returnValue = 2;
+    elseif ($m2 >= 690 && $m2 <= 1170)
+        $returnValue = 2;
+    elseif ($m2 >= 690 && $m2 <= 1170)
+        $returnValue = 2;
+    elseif ($m2 >= 690 && $m2 <= 1170)
+        $returnValue = 2;
+    elseif ($m2 >= 690 && $m2 <= 1170)
+        $returnValue = 2;
+    elseif ($m2 >= 690 && $m2 <= 1170)
+        $returnValue = 2;
+    elseif ($m2 >= 690 && $m2 <= 1170)
+        $returnValue = 2;
+    elseif ($m2 >= 690 && $m2 <= 1170)
+        $returnValue = 2;
+    elseif ($m2 >= 690 && $m2 <= 1170)
+        $returnValue = 2;
+    elseif ($m2 >= 690 && $m2 <= 1170)
+        $returnValue = 2;
+    elseif ($m2 >= 690 && $m2 <= 1170)
+        $returnValue = 2;
+    elseif ($m2 >= 690 && $m2 <= 1170)
+        $returnValue = 2;
+    elseif ($m2 >= 690 && $m2 <= 1170)
+        $returnValue = 2;
+    elseif ($m2 >= 690 && $m2 <= 1170)
+        $returnValue = 2;
+    elseif ($m2 >= 690 && $m2 <= 1170)
+        $returnValue = 2;
+    else
+        $returnValue = 50;
+
+}
+
 ?>
