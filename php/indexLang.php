337e6967554a3bf28423d4ef0654e7dac6ff899e
mnkhbmb <munkhbymba0217@gmail.com>
2017-10-02 02:05:52 +0800

    asd

--- php/indexLang.php
+++ php/indexLang.php
@@ -4,8 +4,8 @@ if (isset($_GET['lang'])) {
         echo '
                         <h4>Уважаемый партнер</h4>
                         <div class="uk-text-justify">
-                        <p><font size="3">Наша компания предлагает геотермальные установки, которые позволяют полностью отказаться от сжигания угля и других углеводородов с целью обеспечения отопление и горячей водой. Мы имеем опыт работы в условиях Монгольского климата и обеспечиваем 5 летний срок гарантии. Данная технология позволяет сделать систему в любом месте независимо от географии, а также нет ограничения по её мощности. Данное приложение позволяет Вам сделать самостоятельный расчет необходимой для Ваших нужд установки, получить ее стоимость, а также оценить последующие эксплуатационные расходы.
-На весь расчет потребуется 3 минут при условии, что у Вас есть все необходимые вводные данные Вашего объекта, такие как общая площадь, количество проживающих.
+                        <p><font size="3">Наша компания предлагает геотермальные установки, которые позволяют полностью отказаться от сжигания угля и других углеводородов с целью обеспечения отопление и горячей водой. Мы имеем опыт работы в условиях Монгольского климата и обеспечиваем 5 летний срок гарантии. Данная технология позволяет сделать систему в любом месте независимо от географии, а также нет ограничения по её мощности. Данное приложение позволяет Вам сделать самостоятельный расчет необходимой для Ваших нужд установки, получить ее стоимость, а также оценить последующие эксплуатационные расходы.<br>
+На весь расчет потребуется 3 минут при условии, что у Вас есть все необходимые вводные данные Вашего объекта, такие как общая площадь, количество проживающих.<br><br>
 Заявки на инсталляцию в этом году (2017) принимаются до 27 октября.   
 Будем рады дальнейшему плодотворному сотрудничеству команда Монгол Тулш</font></p>
 </div>
@@ -21,8 +21,8 @@ if (isset($_GET['lang'])) {
         echo '
                         <h4>Dear Customer</h4>
                         <div class="uk-text-justify">
-                        <p><font size="3">Our company offers geothermal plants that allow to completely abandon the combustion of coal and other hydrocarbons in order to provide heating and hot water. We have experience in the conditions of the Mongolian climate and provide a 5-year warranty period. This technology allows you to make the system anywhere, regardless of geography, and there is no limit to its power. This application allows you to make a self-calculation of the installation necessary for your needs, to obtain its cost, as well as to estimate the subsequent operating costs.
-The entire calculation will take 3 minutes provided that you have all the necessary input data of your facility, such as the total area, the number of people living.
+                        <p><font size="3">Our company offers geothermal plants that allow to completely abandon the combustion of coal and other hydrocarbons in order to provide heating and hot water. We have experience in the conditions of the Mongolian climate and provide a 5-year warranty period. This technology allows you to make the system anywhere, regardless of geography, and there is no limit to its power. This application allows you to make a self-calculation of the installation necessary for your needs, to obtain its cost, as well as to estimate the subsequent operating costs.<br>
+The entire calculation will take 3 minutes provided that you have all the necessary input data of your facility, such as the total area, the number of people living.<br><br>
 Applications for installation this year (2017) are accepted until October 27.
 We will be glad to further fruitful cooperation team Mongol Tulsh
 </font></p>
